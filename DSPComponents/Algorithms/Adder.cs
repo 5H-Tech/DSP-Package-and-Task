48849f2f69092e7938ecb0292574395f8cc88fa4
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-10-21 21:28:12 +0200

    Updating the Adder to Deal With some the edge cases

--- DSPComponents/Algorithms/Adder.cs
+++ DSPComponents/Algorithms/Adder.cs
@@ -16,23 +16,47 @@ namespace DSPAlgorithms.Algorithms
         {
             OutputSignal = new Signal(new List<float>(), false);
 
+           
+            int maxLen = int.MinValue;
+            int maxIdx = 0;
             for (int i = 0; i < InputSignals.Count; i++)
             {
-                if (OutputSignal.Samples.Count == 0)
+                if(InputSignals[i].Samples.Count>=maxLen)
                 {
-                    foreach (float sample in InputSignals[i].Samples)
-                    {
-                        OutputSignal.Samples.Add(sample);
-                    }
-                }
-                else
-                {
-                    for (int k = 0; k < Math.Min(OutputSignal.Samples.Count, InputSignals[i].Samples.Count); k++)
-                    {
-                        OutputSignal.Samples[k] += InputSignals[i].Samples[k];
-                    }
+                    maxLen = InputSignals[i].Samples.Count;
+                    OutputSignal = InputSignals[i];
+                    maxIdx = i;
                 }
             }
+
+            for (int i = 0; i < InputSignals.Count; i++)
+            {
+                if (i == maxIdx)
+                    continue;
+                for (int j = 0; j < InputSignals[i].Samples.Count; j++)
+                {
+                    OutputSignal.Samples[j] += InputSignals[i].Samples[j];
+                }
+            }
+
+
+            //for (int i = 0; i < InputSignals.Count; i++)
+            //{
+            //    if (OutputSignal.Samples.Count == 0)
+            //    {
+            //        foreach (float sample in InputSignals[i].Samples)
+            //        {
+            //            OutputSignal.Samples.Add(sample);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        for (int k = 0; k < Math.Min(OutputSignal.Samples.Count, InputSignals[i].Samples.Count); k++)
+            //        {
+            //            OutputSignal.Samples[k] += InputSignals[i].Samples[k];
+            //        }
+            //    }
+            //}
         }
     }
 }
