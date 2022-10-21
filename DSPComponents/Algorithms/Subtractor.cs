0e8db28aea6c612ea68992fac453c98b5a1a9b0f
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-10-21 16:19:52 +0200

    Multiply Signal By Constant and Subtractor

--- DSPComponents/Algorithms/Subtractor.cs
+++ DSPComponents/Algorithms/Subtractor.cs
@@ -19,7 +19,39 @@ namespace DSPAlgorithms.Algorithms
         /// </summary>
         public override void Run()
         {
-            throw new NotImplementedException();
+            /*OutputSignal = new Signal(new List<float>(), false);
+            MultiplySignalByConstant multiplySignalByConstant = new MultiplySignalByConstant();
+            Adder adder = new Adder();
+            multiplySignalByConstant.InputSignal = InputSignal2;
+            multiplySignalByConstant.InputConstant = -1;
+            multiplySignalByConstant.Run();
+            Adder a = new Adder();
+            a.InputSignals = new List<Signal>();
+            a.InputSignals.Add(InputSignal1);
+            a.InputSignals.Add(multiplySignalByConstant.OutputMultipliedSignal);
+            a.Run();
+            foreach (float sample in a.OutputSignal.Samples)
+            {
+                OutputSignal.Samples.Add(sample);
+            }*/
+            OutputSignal = new Signal(new List<float>(), false);
+            int size = Math.Max(InputSignal1.Samples.Count, InputSignal2.Samples.Count);
+            for (int i = 0; i < size; i++)
+            {
+                if (InputSignal1.Samples.Count <= i)
+                {
+                    OutputSignal.Samples.Add(0 - InputSignal2.Samples[i]);
+                }
+                else if (InputSignal2.Samples.Count <= i)
+                {
+                    OutputSignal.Samples.Add(InputSignal1.Samples[i]);
+                }
+                else
+                {
+                    OutputSignal.Samples.Add(InputSignal1.Samples[i] - InputSignal2.Samples[i]);
+                }
+
+            }
         }
     }
 }
