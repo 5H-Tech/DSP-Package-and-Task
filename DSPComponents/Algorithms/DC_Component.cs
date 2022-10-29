1ad61ad188f11212cf86b6939cdbb20f312feecb
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-10-29 21:43:22 +0200

    Task 2 is Done!!

---
+++ DSPComponents/Algorithms/DC_Component.cs
@@ -0,0 +1,27 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DC_Component: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            float mean = InputSignal.Samples.Average();
+            OutputSignal = new Signal(new List<float>(), false);
+            foreach (var item in InputSignal.Samples)
+            {
+                OutputSignal.Samples.Add(item - mean);
+            }
+
+
+        }
+    }
+}
---
+++ DSPComponents/Algorithms/SinCos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSPAlgorithms.DataStructures;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class SinCos: Algorithm
+    {
+        public string type { get; set; }
+        public float A { get; set; }
+        public float PhaseShift { get; set; }
+        public float AnalogFrequency { get; set; }
+        public float SamplingFrequency { get; set; }
+        public List<float> samples { get; set; }
+        public override void Run()
+        {
+            samples=new List<float>();
+            for (int i = 0; i <SamplingFrequency; i++)
+            {
+                double tmp = (type == "sin" ? A * Math.Sin(2 * Math.PI * i * (AnalogFrequency / SamplingFrequency) + PhaseShift) :
+                                              A * Math.Cos(2 * Math.PI * i * (AnalogFrequency / SamplingFrequency) + PhaseShift));
+                samples.Add((float)tmp);
+
+            }
+            
+        }
+
+    }
+}
